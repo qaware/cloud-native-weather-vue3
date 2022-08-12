77612b47f789db9533b8857277ce31eeaa69c969
Robin Kalleicher <robin.kalleicherqaware.de>
2022-08-12 12:19:11 +0200

    add comments to vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
+    // desired port for your UI
     port: 3000,
     proxy: {
+      // according to our backend api calls start with /api
       '/api': {
+        // the local backend is running on localhost:8080
         target: 'http://localhost:8080',
         ws: true,
         changeOrigin: true
